2250dbf0497a236b335c1f635c6413e8e9f60323
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2017-01-07 04:52:15 +0300

    Math:Added first math function and simple test of it.

---
+++ rmcd/primes.h
@@ -0,0 +1,41 @@
+#ifndef RMATHCALC_PRIMES_H_INCLUDED
+#define RMATHCALC_PRIMES_H_INCLUDED
+
+#include <limits>
+
+namespace math
+{
+  /**
+   * @brief Calculates floor of square root
+   * @param value Value to find floor of square root of
+   * @return Floor of square root
+   *
+   * Finds the integer floor of square root of value, i.e. the maximum integer number
+   * satisfying the condition:
+   * @code
+   * sqrt_floor * sqrt_floor <= value
+   * @endcode
+   */
+  template<typename T>
+  T sqrt_floor(T value)
+  {
+    static_assert(std::numeric_limits<T>::is_integer,
+                  "sqrt_floor is usable only with integer types!");
+    if (0 >= value)
+      return 0;
+    if (3 >= value)
+      return 1;
+
+    T result, devider = value;
+    do
+      {
+        result = devider;
+        devider = ((value / devider + devider) >> 1);
+      }
+    while (result > devider);
+
+    return result;
+  }
+}
+
+#endif // RMATHCALC_PRIMES_H_INCLUDED
--- tests/math_test.cpp
+++ tests/math_test.cpp
@@ -2,9 +2,18 @@
 
 #include "gtest/gtest.h"
 
-TEST(first_test, primitive_test)
+#include "primes.h"
+
+TEST(sqrt_floor, simple_test)
 {
-  int i = 100;
-  int j = 50;
-  EXPECT_EQ(i/j, 2);
+  EXPECT_EQ(math::sqrt_floor(0), 0);
+  EXPECT_EQ(math::sqrt_floor(1), 1);
+  EXPECT_EQ(math::sqrt_floor(2), 1);
+  EXPECT_EQ(math::sqrt_floor(3), 1);
+  EXPECT_EQ(math::sqrt_floor(4), 2);
+  EXPECT_EQ(math::sqrt_floor(120), 10);
+  EXPECT_EQ(math::sqrt_floor(121), 11);
+  EXPECT_EQ(math::sqrt_floor(125), 11);
+  EXPECT_EQ(math::sqrt_floor(143), 11);
+  EXPECT_EQ(math::sqrt_floor(144), 12);
 }
